8e7b7794e0b20136b471f5b83ec62b952df70025
Zetaplx <kylgrg.public@gmail.com>
2026-09-22 17:24:27 -0500

    Starting to do tech disk stuffs

--- Content.Server/_Persistence14/Research/TechDisk/TechDiskSystem.cs
+++ Content.Server/_Persistence14/Research/TechDisk/TechDiskSystem.cs
@@ -1,6 +1,20 @@
+using Content.Shared._Persistence14.Research.TechDisk;
+using Content.Shared.DoAfter;
+using Content.Shared.Interaction;
+
 namespace Content.Server._Persistence14.Research.TechDisk;
 
 public sealed partial class TechDiskSystem : EntitySystem
 {
+    [SubscribeLocalEvent]
+    private void OnInteractWith(EntityUid uid, TechnologyDiskComponent diskComponent, ref AfterInteractEvent args)
+    {
+        
+    }
 
+    [SubscribeLocalEvent]
+    private void OnDoAfterComplete(EntityUid uid, TechnologyDiskComponent diskComponent, ref DoAfterArgs args)
+    {
+        
+    }
 }
